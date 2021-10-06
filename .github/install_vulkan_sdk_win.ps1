b14a9ddf8cb98cab0316911e4749f3f8e4164511
Patrick Dworski <nostritius@googlemail.com>
2021-10-06 19:27:41 +0200

    Add out file parameter for vulkan installation

--- .github/install_vulkan_sdk_win.ps1
+++ .github/install_vulkan_sdk_win.ps1
@@ -2,6 +2,6 @@
     Powershell installation script based on https://github.com/actions/virtual-environments/issues/18
 #>
 
-Invoke-WebRequest -Uri https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe
+Invoke-WebRequest -Uri https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe -OutFile vulkan-sdk.exe
 $installer = Start-Process -FilePath vulkan-sdk.exe -Wait -PassThru -ArgumentList @("/S");
 $installer.WaitForExit();
