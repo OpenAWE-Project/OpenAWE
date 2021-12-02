0d73c358836f7e620157cb0bde8d6b318773d078
Patrick Dworski <nostritius@googlemail.com>
2021-12-03 00:02:39 +0100

    Remove OS dependent code from uuid.cpp

--- src/common/uuid.cpp
+++ src/common/uuid.cpp
@@ -41,28 +41,6 @@ UUID::UUID(const UUID &uuid) {
 UUID UUID::createFromString(const std::string &str) {
 	UUID uuid;
 
-#if OS_MACOS || OS_IOS
-	auto newId = CFUUIDCreateFromString(str);
-	auto bytes = CFUUIDGetUUIDBytes(newId);
-	CFRelease(newId);
-
-	uuid._id[0] = bytes.byte0;
-	uuid._id[1] = bytes.byte1;
-	uuid._id[2] = bytes.byte2;
-	uuid._id[3] = bytes.byte3;
-	uuid._id[4] = bytes.byte4;
-	uuid._id[5] = bytes.byte5;
-	uuid._id[6] = bytes.byte6;
-	uuid._id[7] = bytes.byte7;
-	uuid._id[8] = bytes.byte8;
-	uuid._id[9] = bytes.byte9;
-	uuid._id[10] = bytes.byte10;
-	uuid._id[11] = bytes.byte11;
-	uuid._id[12] = bytes.byte12;
-	uuid._id[13] = bytes.byte13;
-	uuid._id[14] = bytes.byte14;
-	uuid._id[15] = bytes.byte15;
-#else
 	std::string rawStr = std::regex_replace(str, std::regex("[^0-9a-fA-F]"), "");
 
 	if (rawStr.length() != 32)
@@ -71,40 +49,17 @@ UUID UUID::createFromString(const std::string &str) {
 	for (int i = 0; i < 16; ++i) {
 		uuid._id[i] = std::stoi(rawStr.substr(i * 2, 2), nullptr, 16);
 	}
-#endif
 
 	return uuid;
 }
 
 UUID UUID::generateRandom() {
 	UUID uuid;
-#if OS_MACOS || OS_IOS
-	auto newId = CFUUIDCreate(NULL);
-	auto bytes = CFUUIDGetUUIDBytes(newId);
-	CFRelease(newId);
-
-	uuid._id[0] = bytes.byte0;
-	uuid._id[1] = bytes.byte1;
-	uuid._id[2] = bytes.byte2;
-	uuid._id[3] = bytes.byte3;
-	uuid._id[4] = bytes.byte4;
-	uuid._id[5] = bytes.byte5;
-	uuid._id[6] = bytes.byte6;
-	uuid._id[7] = bytes.byte7;
-	uuid._id[8] = bytes.byte8;
-	uuid._id[9] = bytes.byte9;
-	uuid._id[10] = bytes.byte10;
-	uuid._id[11] = bytes.byte11;
-	uuid._id[12] = bytes.byte12;
-	uuid._id[13] = bytes.byte13;
-	uuid._id[14] = bytes.byte14;
-	uuid._id[15] = bytes.byte15;
-#else
 
 	do {
 		std::generate_n(uuid._id, 16, std::mt19937(std::chrono::system_clock::now().time_since_epoch().count()));
 	} while (uuid.isNil());
-#endif
+
 	return uuid;
 }
 
