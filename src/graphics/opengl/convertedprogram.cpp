fd07786176db06463fd538f247ec2a579388875c
Patrick Dworski <nostritius@googlemail.com>
2022-01-24 21:37:25 +0100

    Add missing license header to converted program

--- src/graphics/opengl/convertedprogram.cpp
+++ src/graphics/opengl/convertedprogram.cpp
@@ -1,6 +1,22 @@
-//
-// Created by patrick on 27.12.21.
-//
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <fmt/format.h>
 #include <glm/gtc/type_ptr.hpp>
