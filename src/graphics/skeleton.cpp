5f7e7a20fb2824f7f9d991c1559d02d9cfde821e
Patrick Dworski <nostritius@googlemail.com>
2022-03-19 22:44:30 +0100

    Add skeleton implementation for skeleton based animations

---
+++ src/graphics/skeleton.cpp
@@ -0,0 +1,151 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <memory>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include "src/common/readstream.h"
+#include "src/common/exception.h"
+
+#include "src/graphics/skeleton.h"
+#include "src/graphics/pointlist.h"
+
+#include "src/awe/havokfile.h"
+#include "src/awe/resman.h"
+
+namespace Graphics {
+
+Skeleton::Skeleton(rid_t rid) {
+	std::unique_ptr<Common::ReadStream> havok(ResMan.getResource(rid));
+	if (!havok)
+		throw Common::Exception("Havok file for animation not found with the rid {:x}", rid);
+
+	AWE::HavokFile havokFile(*havok);
+	const auto animationContainer = havokFile.getAnimationContainer();
+	if (animationContainer.skeletons.empty())
+		throw Common::Exception("No animations in havok file");
+
+	const auto skeleton = havokFile.getSkeleton(animationContainer.skeletons[0]);
+	_name = skeleton.name;
+
+	for (const auto &bone : skeleton.bones) {
+		Bone newBone {
+			bone.name,
+			bone.parentIndex,
+			bone.position,
+			bone.rotation
+		};
+
+		_bones.emplace_back(newBone);
+	}
+
+	for (const auto &bone: _bones) {
+		_transformations.insert(std::make_pair(bone.name, glm::identity<glm::mat4>()));
+	}
+}
+
+Skeleton::Skeleton(const Skeleton &skeleton) {
+	_name = skeleton.getName();
+	_bones = skeleton._bones;
+
+	for (const auto &bone: _bones) {
+		_transformations.insert(std::make_pair(bone.name, glm::identity<glm::mat4>()));
+	}
+}
+
+Skeleton &Skeleton::operator=(const Skeleton &skeleton) {
+	_name = skeleton._name;
+	_bones = skeleton._bones;
+
+	for (const auto &bone: _bones) {
+		_transformations.insert(std::make_pair(bone.name, glm::identity<glm::mat4>()));
+	}
+
+	return *this;
+}
+
+void Skeleton::update(const Animation &animation, float time) {
+	std::vector<glm::mat4> animationTransforms(_bones.size());
+
+	for (int i = 0; i < _bones.size(); ++i) {
+		const auto &bone = _bones[i];
+		if (animation.hasTrackForBone(bone.name))
+			// If the animation defines a track for the bone, calculate the transformation
+			animationTransforms[i] = animation.calculateTransformation(bone.name, time);
+		else
+			// If the animation defines no track for the bone, calculate the transformation
+			animationTransforms[i] =
+				glm::translate(bone.translation) *
+				glm::toMat4(bone.rotation);
+	}
+
+	for (int i = 0; i < _bones.size(); ++i) {
+		const auto &bone = _bones[i];
+		auto transformation = glm::identity<glm::mat4>();
+
+		// Move the point back to its child position
+		short parentIndex = bone.parent;
+		while (parentIndex >= 0) {
+			const auto &parentBone = _bones[parentIndex];
+
+			transformation =
+					animationTransforms[parentIndex] *
+					transformation;
+
+			parentIndex = parentBone.parent;
+		}
+
+		// Move the point according to the transformation
+		transformation *= animationTransforms[i];
+
+		// Move the point to the skeleton origin
+		const auto inverseTransform = _inverseTransform.find(bone.name);
+		if (inverseTransform != _inverseTransform.end())
+			transformation *= inverseTransform->second;
+
+		_transformations[bone.name] = transformation;
+	}
+}
+
+const std::string &Skeleton::getName() const {
+	return _name;
+}
+
+std::vector<glm::mat3x4> Skeleton::getSkinningMatrices(std::vector<std::string> indices) const {
+	std::vector<glm::mat3x4> transformation(indices.size());
+	for (int i = 0; i < indices.size(); ++i) {
+		transformation[i] = glm::transpose(_transformations.at(indices[i]));
+	}
+
+	for (int i = 0; i < 64 - transformation.size(); ++i) {
+		transformation.emplace_back(glm::identity<glm::mat3x4>());
+	}
+	
+	return transformation;
+}
+
+void Skeleton::setInverseTransform(const std::map<std::string, glm::mat4> &inverseTransform) {
+	_inverseTransform = inverseTransform;
+}
+
+} // End of namespace Graphics
---
+++ src/graphics/skeleton.h
@@ -0,0 +1,105 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENAWE_SKELETON_H
+#define OPENAWE_SKELETON_H
+
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include "src/awe/types.h"
+
+#include "src/graphics/animation.h"
+#include "src/graphics/pointlist.h"
+#include "linelist.h"
+
+namespace Graphics {
+
+class PointList;
+
+/*!
+ * \brief Class for handling skeletons for skeletal animation
+ *
+ * This class handles a set of bones, used to calculate the current bone transformation matrices of them and provide
+ * them in list of reduced skinning matrices.
+ */
+class Skeleton {
+public:
+	Skeleton() = default;
+
+	/*!
+	 * Load a skeleton from a havok file specifiede by a resource id
+	 *
+	 * \param rid The resource id of the havok file to load
+	 */
+	Skeleton(rid_t rid);
+	Skeleton(const Skeleton &skeleton);
+
+	Skeleton &operator=(const Skeleton &skeleton);
+
+	/*!
+	 * Update the state of the skeleton with a specific animation and a specific time
+	 *
+	 * \param animation The animation with which to modify the skeleton
+	 * \param time The time at which to apply the animation
+	 */
+	void update(const Animation &animation, float time);
+
+	/*!
+	 * Get the name of the skeleton
+	 *
+	 * \return The name of the skeleton
+	 */
+	const std::string &getName() const;
+
+	/*!
+	 * Get the skinning matrices for a certain set of bones
+	 *
+	 * \param indices The bone names to get the skinning matrices for
+	 * \return A vector of the requested skinning matrices
+	 */
+	std::vector<glm::mat3x4> getSkinningMatrices(std::vector<std::string> indices) const;
+
+	/*!
+	 * Set the inverse transform matrices for this skeleton
+	 *
+	 * \param inverseTransform A map which associates a bone name with its inverse transformation matrix
+	 */
+	void setInverseTransform(const std::map<std::string, glm::mat4> &inverseTransform);
+
+private:
+	struct Bone {
+		std::string name;
+		short parent;
+		glm::vec3 translation;
+		glm::quat rotation;
+	};
+
+	std::vector<Bone> _bones;
+	std::map<std::string, glm::mat4> _transformations;
+	std::map<std::string, glm::mat4> _inverseTransform;
+	std::string _name;
+};
+
+} // End of namespace Graphics
+
+#endif //OPENAWE_SKELETON_H
