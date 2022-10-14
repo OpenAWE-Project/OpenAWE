c6a9a9b05606a0b41cc60fc3d5d5a4d3bd598e1b
Patrick Dworski <nostritius@googlemail.com>
2022-10-14 21:32:34 +0200

    Add bitstream class

---
+++ src/common/bitstream.h
@@ -0,0 +1,114 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENAWE_BITSTREAM_H
+#define OPENAWE_BITSTREAM_H
+
+#include <bit>
+
+#include "src/common/readstream.h"
+#include "src/common/endianness.h"
+
+namespace Common {
+
+/*!
+ * \brief Class implementing a stream operating on bits
+ *
+ * This class offers the possiblity to read bits over a normal bitstream.
+ *
+ * A value is being read from a normal readstream depending on the template paremeter given and is then read bit by bit
+ * until we have all requested bits for the value.
+ *
+ * \tparam T The data type on which the bitstream should operate
+ * \tparam le If the bitstream read in little endian
+ * \tparam msb If the stream is read with the most significant or least significant bit first
+ */
+template<typename T, bool le, bool msb>
+class BitStream {
+public:
+	explicit BitStream(Common::ReadStream &bits) : _remainingBits(0), _curValue(0), _bits(bits) {}
+
+	uint32_t read(size_t n) {
+		uint32_t newValue = 0;
+		for (int i = 0; i < n; ++i) {
+			newValue <<= 1;
+			newValue |= (read() ? 1 : 0);
+		}
+		return newValue;
+	}
+
+	void skip(size_t n) {
+		for (int i = 0; i < n; ++i) {
+			read();
+		}
+	}
+
+	bool read() {
+		if (_remainingBits == 0)
+			readNextValue();
+
+		bool value;
+		if constexpr (msb) {
+			value = _curValue & (1 << (sizeof(T) * 8 - 1));
+			_curValue <<= 1;
+		} else {
+			value = _curValue & 1;
+			_curValue >>= 1;
+		}
+
+		_remainingBits--;
+		return value;
+	}
+
+private:
+	void readNextValue() {
+		_bits.read(&_curValue, sizeof(T));
+#ifdef LITTLE_ENDIAN_SYSTEM
+		if constexpr (!le && sizeof(T) > 1)
+			_curValue = swapBytes(_curValue);
+#endif // LITTLE_ENDIAN_SYSTEM
+#ifdef BIG_ENDIAN_SYSTEM
+		if constexpr (le && sizeof(T) > 1)
+			_curValue = swapBytes(_curValue);
+#endif // BIG_ENDIAN_SYSTEM
+		_remainingBits = sizeof(T) * 8;
+	}
+
+	T _curValue;
+	unsigned short _remainingBits;
+	Common::ReadStream &_bits;
+};
+
+typedef BitStream<uint8_t, false, true>   BitStream8MSB;
+typedef BitStream<uint8_t, false, false>  BitStream8LSB;
+
+typedef BitStream<uint16_t, true, true>   BitStream16LEMSB;
+typedef BitStream<uint16_t, true, false>  BitStream16LELSB;
+typedef BitStream<uint16_t, false, true>  BitStream16BEMSB;
+typedef BitStream<uint16_t, false, false> BitStream16BELSB;
+
+typedef BitStream<uint32_t, true, true>   BitStream32LEMSB;
+typedef BitStream<uint32_t, true, false>  BitStream32LELSB;
+typedef BitStream<uint32_t, false, true>  BitStream32BEMSB;
+typedef BitStream<uint32_t, false, false> BitStream32BELSB;
+
+} // End of namespace Common
+
+#endif //OPENAWE_BITSTREAM_H
---
+++ test/test_common_bitstream.cpp
@@ -0,0 +1,257 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+#include <bitset>
+
+#include "src/common/memreadstream.h"
+#include "src/common/bitstream.h"
+#include "src/common/endianness.h"
+
+// 0000 0001 0010 0011
+static const uint8_t kTestData[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+
+constexpr std::bitset<64> kResultData           = {0x0123456789ABCDEF};
+constexpr std::bitset<64> kResultData16LE       = {0x23016745AB89EFCD};
+constexpr std::bitset<64> kResultData32LE       = {0x67452301EFCDAB89};
+constexpr std::bitset<64> kResultDataReversed   = {0x80C4A2E691D5B3F7};
+constexpr std::bitset<64> kResultDataReversed16 = {0xC480E6A2D591F7B3};
+constexpr std::bitset<64> kResultDataReversed32 = {0xE6A2C480F7B3D591};
+
+TEST(BitStream, readBit_8MSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream8MSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultData, result);
+}
+
+TEST(BitStream, readBit_8LSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream8LSB bits(testDataStream);
+
+	std::bitset<64> result;
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultDataReversed, result);
+}
+
+TEST(BitStream, readBits_8MSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream8MSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 0);
+	EXPECT_EQ(bits.read(3), 0);
+	EXPECT_EQ(bits.read(3), 4);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 0);
+}
+
+TEST(BitStream, readBits_8LSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream8LSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 8);
+	EXPECT_EQ(bits.read(3), 0);
+	EXPECT_EQ(bits.read(3), 3);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 1);
+}
+
+TEST(BitStream, readBit_16BEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16BEMSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultData, result);
+}
+
+TEST(BitStream, readBit_16BELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16BELSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultDataReversed16, result);
+}
+
+TEST(BitStream, readBit_16LEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16LEMSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultData16LE, result);
+}
+
+TEST(BitStream, readBit_16LELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16LELSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultDataReversed, result);
+}
+
+TEST(BitStream, readBits_16BEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16BEMSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 0);
+	EXPECT_EQ(bits.read(3), 0);
+	EXPECT_EQ(bits.read(3), 4);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 0);
+}
+
+TEST(BitStream, readBits_16BELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16BELSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 12);
+	EXPECT_EQ(bits.read(3), 2);
+	EXPECT_EQ(bits.read(3), 2);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 0);
+}
+
+TEST(BitStream, readBits_16LEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16LEMSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 2);
+	EXPECT_EQ(bits.read(3), 1);
+	EXPECT_EQ(bits.read(3), 4);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 0);
+}
+
+TEST(BitStream, readBits_16LELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream16LELSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 8);
+	EXPECT_EQ(bits.read(3), 0);
+	EXPECT_EQ(bits.read(3), 3);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 1);
+}
+
+TEST(BitStream, readBit_32BEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32BEMSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultData, result);
+}
+
+TEST(BitStream, readBit_32BELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32BELSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultDataReversed32, result);
+}
+
+TEST(BitStream, readBit_32LEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32LEMSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultData32LE, result);
+}
+
+TEST(BitStream, readBit_32LELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32LELSB bits(testDataStream);
+
+	std::bitset<64> result(0);
+	for (int i = 0; i < 64; ++i)
+		result[63 - i] = bits.read();
+
+	EXPECT_EQ(kResultDataReversed, result);
+}
+
+TEST(BitStream, readBits_32BEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32BEMSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 0);
+	EXPECT_EQ(bits.read(3), 0);
+	EXPECT_EQ(bits.read(3), 4);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 0);
+}
+
+TEST(BitStream, readBits_32BELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32BELSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 14);
+	EXPECT_EQ(bits.read(3), 3);
+	EXPECT_EQ(bits.read(3), 2);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 0);
+}
+
+TEST(BitStream, readBits_32LEMSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32LEMSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 6);
+	EXPECT_EQ(bits.read(3), 3);
+	EXPECT_EQ(bits.read(3), 5);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 1);
+}
+
+TEST(BitStream, readBits_32LELSB) {
+	Common::MemoryReadStream testDataStream(kTestData, sizeof(kTestData));
+	Common::BitStream32LELSB bits(testDataStream);
+
+	EXPECT_EQ(bits.read(4), 8);
+	EXPECT_EQ(bits.read(3), 0);
+	EXPECT_EQ(bits.read(3), 3);
+	bits.skip(2);
+	EXPECT_EQ(bits.read(2), 1);
+}
+
