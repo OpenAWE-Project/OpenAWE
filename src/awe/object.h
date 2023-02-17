ca68528c47369f6784d685981ce4ccd619d3b719
Patrick Dworski <nostritius@googlemail.com>
2023-02-17 13:34:15 +0100

    Add macros to prevent errors from defined far and near variables

--- src/awe/object.h
+++ src/awe/object.h
@@ -9,6 +9,15 @@
 
 #include "src/awe/types.h"
 
+/*
+ * Windows headers define the name far and near for historical purposes. This should be disabled to avoid confusing
+ * compiler errors.
+ */
+#if OS_WINDOWS
+#	undef far
+#	undef near
+#endif
+
 namespace AWE::Templates {
 
 struct Skeleton {
@@ -375,8 +384,7 @@ struct SpotLight {
 	rid_t meshResource;
 	glm::vec3 meshPosition;
 	glm::mat3 meshRotation;
-	float near;
-	float far;
+	float near, far;
 	float depthBias;
 	float depthSlopeBias;
 	float drainMultiplier;
