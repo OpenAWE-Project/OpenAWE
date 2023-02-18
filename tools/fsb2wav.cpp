977e29372851a551e2513feb41a7d21c58eaac62
Patrick Dworski <nostritius@googlemail.com>
2023-02-18 15:03:59 +0100

    Add new tools tex2dds and fsb2wav

---
+++ tools/fsb2wav.cpp
@@ -0,0 +1,61 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdlib>
+
+#include <filesystem>
+
+#include <fmt/format.h>
+#include <CLI/CLI.hpp>
+
+#include "src/common/readfile.h"
+#include "src/common/writefile.h"
+
+#include "src/sound/fsbfile.h"
+#include "src/sound/dumpwav.h"
+
+int main(int argc, char** argv) {
+	CLI::App app("Convert FMOD fsb file to wav file", "fsb2wav");
+
+	std::string binFile;
+
+	app.add_option("fsbarchive", binFile, "The fsb file")
+		->check(CLI::ExistingFile)
+		->required();
+
+	CLI11_PARSE(app, argc, argv);
+
+	std::unique_ptr<Common::ReadStream> binStream = std::make_unique<Common::ReadFile>(binFile);
+	Sound::FSBFile fsb(new Common::ReadFile(binFile));
+
+	for (size_t i = 0; i < fsb.getNumEntries(); ++i) {
+		const std::string path = fsb.getFileName(i);
+
+		fmt::print("{}/{} {}\n", i + 1, fsb.getNumEntries(), path);
+
+		std::unique_ptr<Codecs::SeekableAudioStream> resourceStream(fsb.getStream(i));
+		const auto data = resourceStream->readAll();
+
+		Common::WriteFile wav(path);
+		Sound::dumpWAV(wav, (void *) data.data(), data.size(), 48000, 16, 2);
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ tools/tex2dds.cpp
@@ -0,0 +1,53 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdlib>
+
+#include <filesystem>
+
+#include <fmt/format.h>
+#include <CLI/CLI.hpp>
+
+#include "src/common/readfile.h"
+#include "src/common/writefile.h"
+
+#include "src/graphics/images/tex.h"
+#include "src/graphics/images/dumpdds.h"
+
+int main(int argc, char** argv) {
+	CLI::App app("Convert a tex image to a dds image", "tex2dds");
+
+	std::string texFile, ddsFile;
+
+	app.add_option("teximage", texFile, "The tex image to convert")
+		->check(CLI::ExistingFile)
+		->required();
+
+	CLI11_PARSE(app, argc, argv);
+
+	std::unique_ptr<Common::ReadStream> texStream = std::make_unique<Common::ReadFile>(texFile);
+	Graphics::TEX tex(*texStream);
+
+	Common::WriteFile dds("test.dds");
+	Graphics::dumpDDS(dds, tex);
+	dds.close();
+
+	return EXIT_SUCCESS;
+}
