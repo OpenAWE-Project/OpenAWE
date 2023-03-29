ea5c5db08493e8fde6c5520475a6983c71c154da
Patrick Dworski <nostritius@googlemail.com>
2023-03-29 13:52:27 +0200

    Add foliage depth shader

---
+++ shaders/foliagebillboard-depth.frag.glsl
@@ -0,0 +1,41 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#version 330
+
+uniform sampler2D g_sColorMap;
+uniform sampler2D g_sFoliageNoiseMap;
+
+in vec4 pass_ClipPosition;
+in vec4 pass_ViewPosition;
+in vec2 pass_UV;
+
+layout(location=0) out vec4 out_Depth;
+layout(location=1) out vec4 out_Normal;
+
+void main() {
+    vec4 vColor = texture(g_sColorMap, pass_UV);
+    float fAlpha = vColor.a;
+
+    if (fAlpha - 0.5 < 0.0)
+        discard;
+
+    out_Depth = vec4(pass_ClipPosition.zzz / pass_ClipPosition.w, 1.0);
+}
---
+++ shaders/foliagebillboard-depth.vert.glsl
@@ -0,0 +1,65 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#version 330
+
+struct FoliageMesh {
+    vec2 vAtlasPosition;
+    vec2 vAtlasSize;
+    vec2 vBillboardSize;
+};
+
+uniform FoliageMesh g_foliageMeshTable[48];
+
+uniform mat4 g_mLocalToView;
+uniform mat4 g_mViewToClip;
+
+in vec3 in_Position;
+in int in_Color0;
+
+out vec4 pass_ClipPosition;
+out vec4 pass_ViewPosition;
+out vec2 pass_UV;
+
+void main() {
+    int iFoliageMesh = in_Color0;
+    vec3 vPosition = in_Position;
+
+    float fUpFactor = float(gl_VertexID % 4 == 1) + float(gl_VertexID % 4 == 2);
+    float fSideFactor = -((gl_VertexID + 1) % 2) + gl_VertexID % 2;
+
+    float fEvenFactor = (gl_VertexID + 1) % 2;
+
+    FoliageMesh foliageMesh = g_foliageMeshTable[iFoliageMesh];
+
+    vPosition = (g_mLocalToView * vec4(vPosition, 1.0)).xyz;
+    pass_ViewPosition = vec4(vPosition, 1.0);
+    vPosition.x += fSideFactor * foliageMesh.vBillboardSize.x / 2;
+    vPosition.y += fUpFactor * foliageMesh.vBillboardSize.y;
+
+    vec2 vUV = foliageMesh.vAtlasPosition;
+    vUV.s += fEvenFactor * foliageMesh.vAtlasSize.s;
+    vUV.t += (1 - fUpFactor) * foliageMesh.vAtlasSize.t;
+
+    pass_UV = vUV;
+
+    pass_ClipPosition = g_mViewToClip * vec4(vPosition, 1.0);
+    gl_Position = pass_ClipPosition;
+}
