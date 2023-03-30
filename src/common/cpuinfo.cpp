9923ac0d07f3db2164e236c7b8860bc17372351a
Patrick Dworski <nostritius@googlemail.com>
2023-03-30 19:45:59 +0200

    Add functions to common to retrieve cpu info and features

---
+++ src/common/cpuinfo.cpp
@@ -0,0 +1,80 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstring>
+#include <cstdint>
+
+#if HAS_CPUID_H
+#	include <cpuid.h>
+#endif
+
+#include "src/common/cpuinfo.h"
+#include "src/common/strutil.h"
+
+namespace Common {
+
+std::string getCPUVendor() {
+#if HAS_CPUID_H
+	uint32_t data[4];
+
+	__cpuid(0, data[0], data[1], data[2], data[3]);
+
+	std::string name;
+	name.resize(12);
+
+	std::memcpy(name.data(), &data[1], 4);
+	std::memcpy(name.data() + 4, &data[3], 4);
+	std::memcpy(name.data() + 8, &data[2], 4);
+
+	return name;
+#else
+	return "";
+#endif
+}
+
+std::string getCPUName() {
+#if HAS_CPUID_H
+	uint32_t data[12];
+
+	__cpuid(0x80000002, data[0], data[1], data[2], data[3]);
+	__cpuid(0x80000003, data[4], data[5], data[6], data[7]);
+	__cpuid(0x80000004, data[8], data[9], data[10], data[11]);
+
+	std::string name(reinterpret_cast<char*>(data));
+
+	return name;
+#else
+	return "";
+#endif
+}
+
+bool hasSSE2() {
+#if HAS_CPUID_H
+	uint32_t data[4];
+
+	__cpuid(1, data[0], data[1], data[2], data[3]);
+
+	return data[3] & bit_SSE2;
+#else
+	return false;
+#endif
+}
+
+}
---
+++ src/common/cpuinfo.h
@@ -0,0 +1,45 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENAWE_CPUINFO_H
+#define OPENAWE_CPUINFO_H
+
+#include <string>
+
+namespace Common {
+
+/*!
+ * Get the vendor of the executed cpu using the cpuid opcode. If the vendor retrieval is not possible or supported,
+ * return an empty string.
+ * \return The current cpu vendor
+ */
+std::string getCPUVendor();
+
+std::string getCPUName();
+
+/*!
+ * Return if the cpu supports SSE2 instructions
+ * \return If the cpu supports SSE2 instructions
+ */
+bool hasSSE2();
+
+} // End of namespace Common
+
+#endif //OPENAWE_CPUINFO_H
