a8c53e9b6401698f1b014061d2ebfd08c210893b
Patrick Dworski <nostritius@googlemail.com>
2023-04-30 23:37:42 +0200

    Add physics kinematic character controller

---
+++ src/physics/charactercontroller.cpp
@@ -0,0 +1,92 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glm/gtx/quaternion.hpp>
+
+#include "src/physics/physicsman.h"
+#include "src/physics/charactercontroller.h"
+
+namespace Physics {
+
+CharacterController::CharacterController(float capsuleHeight, float capsuleRadius) :
+	_ghostObject(new btPairCachingGhostObject),
+	_shape(std::make_unique<btCapsuleShape>(capsuleRadius, capsuleHeight)) {
+	_ghostObject->setCollisionFlags(btCollisionObject::CF_CHARACTER_OBJECT);
+	setCollisionObject(_ghostObject);
+
+	btTransform offset;
+	offset.setIdentity();
+	offset.setOrigin(btVector3(0.0f, capsuleHeight / 2.0f + capsuleRadius, 0.0f));
+	setOffset(offset);
+
+	_groundOffset = capsuleHeight / 2.0f + capsuleRadius;
+
+	setCollisionShape(_shape.get());
+
+	_characterController = new btKinematicCharacterController(
+		_ghostObject,
+		_shape.get(),
+		1.0f
+	);
+	_characterController->setGravity(btVector3(0.0f, -1.0f, 0.0f));
+	_characterController->setFallSpeed(100.0f);
+	_characterController->setJumpSpeed(100.0f);
+	_characterController->setUseGhostSweepTest(false);
+
+	setActive(true);
+}
+
+void CharacterController::setLinearVelocity(glm::vec3 direction) {
+	_characterController->setLinearVelocity(btVector3(direction.x, direction.y, direction.z) * 0.05);
+}
+
+void CharacterController::setAngularVelocity(float factor) {
+	_characterController->setAngularVelocity(btVector3(0.0, factor, 0.0));
+}
+
+void CharacterController::jump() {
+	if (_characterController->onGround())
+		_characterController->jump();
+}
+
+glm::vec3 CharacterController::getPosition() {
+	btVector3 origin = _ghostObject->getWorldTransform().getOrigin();
+	return glm::vec3(origin.x(), origin.y() - _groundOffset, origin.z());
+}
+
+glm::mat3 CharacterController::getRotation() {
+	btQuaternion rotation = _ghostObject->getWorldTransform().getRotation();
+	return glm::toMat3(glm::quat(rotation.w(), rotation.x(), rotation.y(), rotation.z()));
+}
+
+void CharacterController::setActive(bool active) {
+	if (active) {
+		PhysicsMan.add(
+			_ghostObject,
+			btBroadphaseProxy::CharacterFilter,
+			btBroadphaseProxy::StaticFilter | btBroadphaseProxy::DefaultFilter
+		);
+		PhysicsMan.add(_characterController);
+	} else {
+		PhysicsMan.remove(_ghostObject);
+	}
+}
+
+} // End of namespace Physics
---
+++ src/physics/charactercontroller.h
@@ -0,0 +1,96 @@
+/* OpenAWE - A reimplementation of Remedys Alan Wake Engine
+ *
+ * OpenAWE is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * OpenAWE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * OpenAWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenAWE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENAWE_CHARACTERCONTROLLER_H
+#define OPENAWE_CHARACTERCONTROLLER_H
+
+#include <btBulletDynamicsCommon.h>
+#include <btBulletCollisionCommon.h>
+#include <BulletDynamics/Character/btKinematicCharacterController.h>
+#include <BulletCollision/CollisionDispatch/btGhostObject.h>
+
+#include "src/physics/collisionobject.h"
+
+namespace Physics {
+
+class CharacterController;
+
+typedef std::shared_ptr<CharacterController> CharacterControllerPtr;
+
+/*!
+ * \brief Class for representing controllable elements in the scene
+ *
+ * This class represents a movable physics object whose velocity can be controlled to achieve a movement in the world.
+ * It reacts furthermore to the gravity of the world.
+ */
+class CharacterController : public CollisionObject {
+public:
+	CharacterController(float capsuleHeight, float capsuleRadius);
+
+    /*!
+     * Set the linear velocity which is a force moving the object to a certain direction
+     *
+     * \param direction The direction in which the velocity shall move
+     */
+	void setLinearVelocity(glm::vec3 direction);
+
+    /*!
+     * Set the angular velocity which is the force rotating the object around the z Axis.
+     *
+     * \param factor The signed factor in which to rate around the up axis
+     */
+	void setAngularVelocity(float factor);
+
+    /*!
+     * Make the character jump. Jumping only works if the character is on the ground
+     */
+	void jump();
+
+    /*!
+     * Set the controller active or inactive
+     *
+     * \param active If the controller should be set active
+     */
+	void setActive(bool active) override;
+
+    /*!
+     * Get the current position of the character
+     *
+     * \return The current character position as vec3
+     */
+	glm::vec3 getPosition();
+
+    /*!
+     * Get the current rotation of the character
+     *
+     * \return The current rotation as 3x3 matrix
+     */
+	glm::mat3 getRotation();
+
+private:
+	float _groundOffset;
+	btPairCachingGhostObject *_ghostObject;
+	std::unique_ptr<btCapsuleShape> _shape;
+	btKinematicCharacterController *_characterController;
+};
+
+} // End of namespace Physics
+
+#endif //OPENAWE_CHARACTERCONTROLLER_H
