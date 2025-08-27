377941035f91e0c4841e93f959e88b4d8ddd487f
Patrick Dworski <nostritius@googlemail.com>
2025-08-27 16:37:48 +0200

    Add real spherical harmonics calculation

--- src/common/sh.h
+++ src/common/sh.h
@@ -45,10 +45,10 @@ namespace Common {
 std::complex<double> shBasisFunc(int l, int m, double theta, double phi) {
 	const double normalization = std::sqrt(
 			((2.0 * l + 1) / (4 * std::numbers::pi)) * boost::math::factorial<double>(l - std::abs(m)) /
-			boost::math::factorial<double>(l + std::abs(m)));
+			boost::math::factorial<double>(l + m));
 
 	// TODO Replace this with std::assoc_legendre once it is supported in Clang and Apple Clang
-	double p = boost::math::legendre_p(l, std::abs(m), std::cos(theta));
+	double p = boost::math::legendre_p(l, m, std::cos(theta));
 
 	if (m < 0)
 		p *= std::pow(-1, m);
@@ -76,6 +76,64 @@ std::complex<double> shBasisFunc(int l, int m, glm::vec3 dir) {
 	const auto phi = std::acos(dir.y / r);
 
 	return shBasisFunc(l, m, theta, phi);
+}
+
+/*!
+ * Calculate the real basis value for spherical harmonics using a certain degree l an order m, a longitude/azimutal
+ * angle theta and the latitude angle phi. The whole equation is described below:
+ * \f[
+ * Y_l^m=\begin{cases}
+ * \sqrt{2} K_l^m cos(m\phi)P_l^{m}(cos(\theta)) & m>0\\
+ * \sqrt{2} K_l^m sin(\vert m\vert\phi)P_l^{\vert m\vert}(cos(\theta)) & m<0\\
+ * K_l^0P_l^0(cos(\theta)) & m=0
+ * \end{cases}
+ * \f]
+ * It is based on the description of \url https://www.cse.chalmers.se/~uffe/xjobb/Readings/GlobalIllumination/Spherical%20Harmonic%20Lighting%20-%20the%20gritty%20details.pdf
+ * and the modifications made there.
+ *
+ * \param l The degree of the function, must be \f$l \geq 0\f$
+ * \param m The order which has to be \f$|m|\leq l\f$
+ * \param theta The longitude/azimutal angle in radians
+ * \param phi The latitude angle in radians
+ * \return The basis function value
+ */
+double shRealBasisFunc(int l, int m, double theta, double phi) {
+	double p = 0.0;
+
+	if (m > 0)
+		p = std::numbers::sqrt2 * std::pow(-1, m) * shBasisFunc(l, std::abs(m), theta, phi).imag();
+	else if (m < 0)
+		p = std::numbers::sqrt2 * std::pow(-1, m) * shBasisFunc(l, std::abs(m), theta, phi).real();
+	else
+		p = shBasisFunc(l, 0, theta, phi).real();
+
+	return p;
+}
+
+/*!
+* Calculate the real basis value for spherical harmonics using a certain degree l an order m, a longitude/azimutal
+ * angle theta and the latitude angle phi. The whole equation is described below:
+ * \f[
+ * Y_l^m=\begin{cases}
+ * \sqrt{2} K_l^m cos(m\phi)P_l^{m}(cos(\theta)) & m>0\\
+ * \sqrt{2} K_l^m sin(\vert m\vert\phi)P_l^{\vert m\vert}(cos(\theta)) & m<0\\
+ * K_l^0P_l^0(cos(\theta)) & m=0
+ * \end{cases}
+ * \f]
+ * It is based on the description of \url https://www.cse.chalmers.se/~uffe/xjobb/Readings/GlobalIllumination/Spherical%20Harmonic%20Lighting%20-%20the%20gritty%20details.pdf
+ * and the modifications made there.
+ *
+ * \param l The degree of the function, must be \f$ l \geq 0\f$
+ * \param m The order which has to be \f$|m|\leq l\f$
+ * \param dir The direction of the basis function to generate
+ * \return The basis function value
+ */
+double shRealBasisFunc(int l, int m, glm::vec3 dir) {
+	const auto r = glm::length(dir);
+	const auto theta = std::atan2(dir.z, dir.x);
+	const auto phi = std::acos(dir.y / r);
+
+	return shRealBasisFunc(l, m, theta, phi);
 }
 
 } // End of namespace Common
